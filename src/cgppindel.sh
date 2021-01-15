f49358c12085940996a6ff1c3ccc90b34570bc45
addy81 <adriana.tou@gmail.com>
2021-01-15 15:07:23 +0000

    added fai input and specified vcf as separate output

--- src/cgppindel.sh
+++ src/cgppindel.sh
@@ -11,16 +11,23 @@ main() {
     echo "Value of genes: '$genes'"
     echo "Value of unmatched: '$unmatched'"
     echo "Value of assembly: '$assembly'"
-    echo "Value of species: '$species'"
     echo "Value of seqtype: '$seqtype'"
     echo "Value of filter: '$filter'"
     echo "Value of tumour: '$tumour'"
     echo "Value of normal: '$normal'"
     
+    # Create input/output directories
     mkdir input
     mkdir -p out/cgppindel_output
+    mkdir -p out/output_vcf
+    mkdir -p out/vcf_index
+
+    # Make the output directory writeable for the app to work.
+    chmod 777 out/cgppindel_output
 
     dx-download-all-inputs
+
+    #Add all inputs in the same folder to enable indeces to be found.
     find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/input
 
     # Unzip fasta reference
@@ -36,7 +43,7 @@ main() {
     sudo docker load --input ~/input/$docker_image_name 
     cgppindel_id=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^quay.io" | cut -d' ' -f2) 
    
-
+    # Run docker image using id
     sudo docker run -v `pwd`:/data -w "/data/out/cgppindel_output" $cgppindel_id \
     pindel.pl \
     -reference /data/input/${reference_prefix}fa \
@@ -52,8 +59,12 @@ main() {
     -outdir /data/out/cgppindel_output
 
 
+    # Move vcf and index in specified runfolder to enable downstream use
+    mv out/cgppindel_output/*.flagged.vcf.gz out/output_vcf
+    mv out/cgppindel_output/*.flagged.vcf.gz.tbi out/vcf_index
 
-    # upload output files
+
+    # Upload output files
     dx-upload-all-outputs
 
     echo "Upload Complete"
