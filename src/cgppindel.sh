258775c6ffa0c2a906ef2bbe14d42146d19bacc8
addy81 <adriana.tou@gmail.com>
2021-01-15 11:36:30 +0000

    initial attempt

--- src/cgppindel.sh
+++ src/cgppindel.sh
@@ -27,69 +27,44 @@ main() {
     echo "Value of filter: '$filter'"
     echo "Value of tumour: '$tumour'"
     echo "Value of normal: '$normal'"
-    echo "Value of outdir: '$outdir'"
+    
+    mkdir input
+    mkdir -p out/cgppindel_output
 
-    # The following line(s) use the dx command-line tool to download your file
-    # inputs to the local file system using variable names for the filenames. To
-    # recover the original filenames, you can use the output of "dx describe
-    # "$variable" --name".
+    dx-download-all-inputs
+    find ~/in -type f -name "*" -print0 | xargs -0 -I {} mv {} ~/input
 
-    dx download "$reference" -o reference
+    # Unzip fasta reference
+    gzip -d ~/input/$reference_name
 
-    dx download "$simrep" -o simrep
 
-    dx download "$genes" -o genes
+    # add dnanexus user to docker group & start docker daemon
+    sudo usermod -a -G docker dnanexus
+    newgrp docker
+    sudo systemctl start docker
 
-    dx download "$unmatched" -o unmatched
+    # load local container & get id
+    sudo docker load --input ~/input/$docker_image 
+    cgppindel_id=$(docker images --format="{{.Repository}} {{.ID}}" | grep "^quay.io" | cut -d' ' -f2) 
+   
 
-    dx download "$filter" -o filter
+    sudo docker run -it -v `pwd`:/data -w "/data/out/cgppindel_output" $cgppindel_id \
+    -reference /data/input/${reference_prefix}.fa \
+    -simrep /data/input/$simrep_name \
+    -genes /data/input/$genes_name \
+    -unmatched /data/input/$unmatched_name \
+    -assembly $assembly \
+    -species Human \
+    -seqtype $seqtype \
+    -filter /data/input/$filter_name \
+    -tumour /data/input/$tumour_name \
+    -normal /data/input/$normal_name \
+    -outdir /data/out/cgpindel_output
 
-    dx download "$tumour" -o tumour
 
-    dx download "$normal" -o normal
 
-    # Fill in your application code here.
-    #
-    # To report any recognized errors in the correct format in
-    # $HOME/job_error.json and exit this script, you can use the
-    # dx-jobutil-report-error utility as follows:
-    #
-    #   dx-jobutil-report-error "My error message"
-    #
-    # Note however that this entire bash script is executed with -e
-    # when running in the cloud, so any line which returns a nonzero
-    # exit code will prematurely exit the script; if no error was
-    # reported in the job_error.json file, then the failure reason
-    # will be AppInternalError with a generic error message.
+    # upload output files
+    dx-upload-all-outputs
 
-    # The following line(s) use the dx command-line tool to upload your file
-    # outputs after you have created them on the local file system.  It assumes
-    # that you have used the output field name for the filename for each output,
-    # but you can change that behavior to suit your needs.  Run "dx upload -h"
-    # to see more options to set metadata.
-
-    output_vcf=$(dx upload output_vcf --brief)
-    vcf_index=$(dx upload vcf_index --brief)
-    wt_bam=$(dx upload wt_bam --brief)
-    wt_bai=$(dx upload wt_bai --brief)
-    wt_bam_md5=$(dx upload wt_bam_md5 --brief)
-    mt_bam=$(dx upload mt_bam --brief)
-    mt_bai=$(dx upload mt_bai --brief)
-    mt_bam_md5=$(dx upload mt_bam_md5 --brief)
-    out_bed=$(dx upload out_bed --brief)
-
-    # The following line(s) use the utility dx-jobutil-add-output to format and
-    # add output variables to your job's output as appropriate for the output
-    # class.  Run "dx-jobutil-add-output -h" for more information on what it
-    # does.
-
-    dx-jobutil-add-output output_vcf "$output_vcf" --class=file
-    dx-jobutil-add-output vcf_index "$vcf_index" --class=file
-    dx-jobutil-add-output wt_bam "$wt_bam" --class=file
-    dx-jobutil-add-output wt_bai "$wt_bai" --class=file
-    dx-jobutil-add-output wt_bam_md5 "$wt_bam_md5" --class=file
-    dx-jobutil-add-output mt_bam "$mt_bam" --class=file
-    dx-jobutil-add-output mt_bai "$mt_bai" --class=file
-    dx-jobutil-add-output mt_bam_md5 "$mt_bam_md5" --class=file
-    dx-jobutil-add-output out_bed "$out_bed" --class=file
+    echo "Upload Complete"
 }
